32b57a3209dfb7bd094626ee40bbd0ac9e596fb7
Blondeau Stephanie <stephanie.blondeau@bts-malraux.net>
2014-09-09 09:01:40 +0000

    Ajout affichage Test

--- src/projetPackage/Test.java
+++ src/projetPackage/Test.java
@@ -5,8 +5,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
+		//Affichage d'un message de bienvenue
+
+		System.out.print("Bonjours");
 	}
 
 }
